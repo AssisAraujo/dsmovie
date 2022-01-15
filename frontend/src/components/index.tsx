fb17124411fbc672f08ce347f156ad7a13f0aca0
Assis Souza Araujo <54477660+AssisAraujo@users.noreply.github.com>
2022-01-14 23:28:58 -0400

    Navbar

---
+++ frontend/src/components/index.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './Navbar/styles.css';
+
+function Navbar() {
+
+    return(
+        <header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>            
+                <a href="https://github.com/AssisAraujo" target="blank" rel="AssisAraujo">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/devsuperior</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    );
+      
+}
+
+
+export default Navbar;
