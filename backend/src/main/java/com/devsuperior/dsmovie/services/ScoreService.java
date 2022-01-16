f857d03ed232f46e8d0ad4d5349f5bd6b267bb9f
Assis Souza Araujo <54477660+AssisAraujo@users.noreply.github.com>
2022-01-16 07:29:25 -0400

    Class ScoreService

--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -2,8 +2,9 @@ package com.devsuperior.dsmovie.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
-
+import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.entities.Score;
@@ -12,31 +13,30 @@ import com.devsuperior.dsmovie.repositories.MovieRepository;
 import com.devsuperior.dsmovie.repositories.ScoreRepository;
 import com.devsuperior.dsmovie.repositories.UserRepository;
 
+@Service
 public class ScoreService {
 
 	@Autowired
 	private MovieRepository movieRepository;
-
+	
 	@Autowired
 	private UserRepository userRepository;
-
+	
 	@Autowired
 	private ScoreRepository scoreRepository;
-
+	
 	@Transactional
-	public void  saveScore(ScoreDTO dto) {
-
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
 		User user = userRepository.findByEmail(dto.getEmail());
-
 		if (user == null) {
-
 			user = new User();
 			user.setEmail(dto.getEmail());
 			user = userRepository.saveAndFlush(user);
 		}
-
-		Movie movie = movieRepository.findById(dto.getMovieId()).get();
 		
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+	
 		Score score = new Score();
 		score.setMovie(movie);
 		score.setUser(user);
@@ -56,9 +56,8 @@ public class ScoreService {
 		
 		movie = movieRepository.save(movie);
 		
-		
-		
-
+		return new MovieDTO(movie);
 	}
-
+	
+	
 }
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,26 @@
 import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
-import './Navbar/styles.css';
+import '.styles.css';
 
 function Navbar() {
 
-    return(
-        <header>
+    return (
+      <header>
         <nav className="container">
-            <div className="dsmovie-nav-content">
-                <h1>DSMovie</h1>            
-                <a href="https://github.com/AssisAraujo" target="blank" rel="AssisAraujo">
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
-                    </div>
-                </a>
-            </div>
+          <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a
+              href="https://github.com/AssisAraujo"
+              target="blank"
+              rel="AssisAraujo"
+            >
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/Dev-Assis</p>
+              </div>
+            </a>
+          </div>
         </nav>
-    </header>
-
+      </header>
     );
       
 }
