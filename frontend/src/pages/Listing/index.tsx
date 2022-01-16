10f3fb61d3cbd02d612945de058cd38ee04efaba
Assis Souza Araujo <54477660+AssisAraujo@users.noreply.github.com>
2022-01-16 13:52:03 -0400

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  // FORMA ERRADA - TESTE
+
+  axios.get(`${BASE_URL}/Movies?size=12&page=1`).then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
-
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
